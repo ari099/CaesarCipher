4a977864b63a509f4ecfbc6b9891890e4319597e
Ariel Lorenzo <ariellorenzo08@gmail.com>
2019-09-06 17:37:22 -0400

    Encryption function works so far. Must set slider to only the number of values in the alphabet

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
 import sys, string
 from PyQt5 import QtWidgets, uic
 
+def rotateLeft(array, k):
+   """
+   Rotate a list to the left
+   :param k:
+   """
+   return array[abs(k):] + array[:abs(k)]
+   
+def rotateRight(array, k):
+   """
+   Rotate a list to the right
+   :param k:
+   """
+   return array[-abs(k):] + array[:-abs(k)]
+
+def encryptString(text, key):
+   """
+   Encrypt a string the Caesar cipher
+   :param text:
+   :param key:
+   """
+   alphabet = list(string.ascii_lowercase)
+   cipher_alphabet = rotateLeft(list(string.ascii_lowercase), key)
+   for i in range(0, len(text)):
+      isUpperCase = False
+      # Find the letter character in the cipher alphabet and get it's index
+      if text[i].lower() in alphabet:
+         if text[i].isupper(): isUpperCase = True
+         index = alphabet.index(text[i].lower()) # Get the index used for it in the alphabet list
+         text_list = list(text) # Turn the text into a list to manipulate it
+         text_list[i] = cipher_alphabet[index] if isUpperCase == False else cipher_alphabet[index].upper() # Replace the letter with the cipher alphabet equivalent
+         text = "".join(text_list) # Turn the text back into a string
+   
+   return text # Return the cipher text
+
+
+
 class Queue(object):
    """
    Simple queue data structure
@@ -30,37 +66,24 @@ class Ui(QtWidgets.QMainWindow):
 
       # Find the slider with the name "CipherDial"
       self.slider = self.findChild(QtWidgets.QSlider, 'CipherDial')
-      self.slider.valueChanged.connect(self.encryptString)
+      self.slider.valueChanged.connect(self.sliderMoved)
 
       # Find the textbox labeled with "UserInput"
       self.textbox = self.findChild(QtWidgets.QTextEdit, 'UserText')
       self.alphabet = list(string.ascii_lowercase)
 
+      # Find the label labeled (hehe) with "CipherTextOutput"
+      self.cipherTextOutput = self.findChild(QtWidgets.QLabel, 'CipherTextOutput')
+
       self.show()
    
-   def encryptString(self):
+   def sliderMoved(self):
       # This is executed when the slider is moved
       # self.textbox.setText(str(self.slider.value()))
       key = self.slider.value()
       user_text = self.textbox.toPlainText()
-      print(str(self.rotateLeft(self.alphabet, key)))
-      # if len(user_text) > 0:
-      #    new_list = self.rotateLeft(self.alphabet, key)
-      #    for character in user_text
-   
-   def rotateLeft(self, array, k):
-      """
-      Rotate a list to the left
-      :param k:
-      """
-      return array[abs(k):] + array[:abs(k)]
-   
-   def rotateRight(self, array, k):
-      """
-      Rotate a list to the right
-      :param k:
-      """
-      return array[-abs(k):] + array[:-abs(k)]
+      if len(user_text) > 0:
+         self.cipherTextOutput.setText(encryptString(user_text, key))
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
