8d5f1a057f9cc31634a5c6b096361c6a506ac338
Ariel Lorenzo <ariellorenzo08@gmail.com>
2019-09-07 20:05:58 -0400

    Moved rotateLeft, rotateRight, and encryptString into the Ui class. Removed encryption_tests.py file.

--- app.py
+++ app.py
@@ -1,41 +1,6 @@
 import sys, string
 from PyQt5 import QtWidgets, uic
 
-def rotateLeft(array, k):
-   """
-   Rotate a list to the left
-   :param k:
-   """
-   return array[abs(k):] + array[:abs(k)]
-   
-def rotateRight(array, k):
-   """
-   Rotate a list to the right
-   :param k:
-   """
-   return array[-abs(k):] + array[:-abs(k)]
-
-def encryptString(text, key):
-   """
-   Encrypt a string the Caesar cipher
-   :param text:
-   :param key:
-   """
-   alphabet = list(string.ascii_lowercase)
-   cipher_alphabet = rotateLeft(list(string.ascii_lowercase), key)
-   for i in range(0, len(text)):
-      isUpperCase = False
-      # Find the letter character in the cipher alphabet and get it's index
-      if text[i].lower() in alphabet:
-         if text[i].isupper(): isUpperCase = True
-         index = alphabet.index(text[i].lower()) # Get the index used for it in the alphabet list
-         text_list = list(text) # Turn the text into a list to manipulate it
-         text_list[i] = cipher_alphabet[index] if isUpperCase == False else cipher_alphabet[index].upper() # Replace the letter with the cipher alphabet equivalent
-         text = "".join(text_list) # Turn the text back into a string
-   
-   return text # Return the cipher text
-
-
 class Ui(QtWidgets.QMainWindow):
    def __init__(self):
       super(Ui, self).__init__()
@@ -58,6 +23,41 @@ class Ui(QtWidgets.QMainWindow):
 
       self.show()
    
+   def rotateLeft(self, array, k):
+      """
+      Rotate a list to the left
+      :param k:
+      """
+      return array[abs(k):] + array[:abs(k)]
+      
+   def rotateRight(self, array, k):
+      """
+      Rotate a list to the right
+      :param k:
+      """
+      return array[-abs(k):] + array[:-abs(k)]
+
+   def encryptString(self, text, key):
+      """
+      Encrypt a string the Caesar cipher
+      :param text:
+      :param key:
+      """
+      alphabet = list(string.ascii_lowercase)
+      cipher_alphabet = self.rotateLeft(list(string.ascii_lowercase), key)
+      for i in range(0, len(text)):
+         isUpperCase = False
+         # Find the letter character in the cipher alphabet and get it's index
+         if text[i].lower() in alphabet:
+            if text[i].isupper(): isUpperCase = True
+            index = alphabet.index(text[i].lower()) # Get the index used for it in the alphabet list
+            text_list = list(text) # Turn the text into a list to manipulate it
+            text_list[i] = cipher_alphabet[index] if isUpperCase == False else cipher_alphabet[index].upper() # Replace the letter with the cipher alphabet equivalent
+            text = "".join(text_list) # Turn the text back into a string
+      
+      return text # Return the cipher text
+   
+   # ############################# UI CONTROL METHODS #############################
    def sliderMoved(self):
       # This is executed when the slider is moved
       # self.textbox.setText(str(self.slider.value()))
@@ -65,14 +65,14 @@ class Ui(QtWidgets.QMainWindow):
       user_text = self.textbox.toPlainText()
       self.sliderNumber.setText("Key: " + str(key))
       if len(user_text) > 0:
-         self.cipherTextOutput.setText(encryptString(user_text, key))
+         self.cipherTextOutput.setText(self.encryptString(user_text, key))
    
    def userInputChanged(self):
       key = self.slider.value()
       user_text = self.textbox.toPlainText()
       if len(user_text) == 0:
           self.cipherTextOutput.setText("Cipher text should appear here....")
-      else: self.cipherTextOutput.setText(encryptString(user_text, key))
+      else: self.cipherTextOutput.setText(self.encryptString(user_text, key))
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
